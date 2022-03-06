ac884a9e6e383573ee3135eaa45a0174a5f36742
hwsingh27 <hwsingh2702@gmail.com>
2022-03-06 14:05:50 -0700

    A7-Part-2

---
+++ base_external/package/ldd/ldd.mk
@@ -0,0 +1,22 @@
+
+LDD_VERSION = 5b9dcc97309c53db61086ebb8fef3dc306437c32
+# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+# with ssh keys and the automated build/test system.
+# Your site should start with git@github.com:
+LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-hwsingh27.git
+LDD_SITE_METHOD = git
+LDD_GIT_SUBMODULES = YES
+LDD_MODULE_SUBDIRS = scull/
+LDD_MODULE_SUBDIRS += misc-modules/
+
+define LDD_INSTALL_TARGET_CMDS
+
+	$(INSTALL) -m 0755 $(@D)/scull/scull_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/scull/scull_unload $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/misc-modules/module_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/usr/bin
+
+endef
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
